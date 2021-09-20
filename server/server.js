32da39701860eff666209e203933ce0cb6f11306
Anurag Gupta <54763478+Anurag5086@users.noreply.github.com>
2021-09-20 10:10:21 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://localhost:27017/explorext", {
+  .connect("mongodb://anurag:test@cluster0-shard-00-00.qdcqc.mongodb.net:27017,cluster0-shard-00-01.qdcqc.mongodb.net:27017,cluster0-shard-00-02.qdcqc.mongodb.net:27017/explorext?ssl=true&replicaSet=atlas-eqn8rl-shard-0&authSource=admin&retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
